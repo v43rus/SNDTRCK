607ec82dee840e42a5ad4bfa18140b086711d497
v43rus <leonard.jm@live.com>
2024-04-23 12:07:50 +0200

    Setup for Admin area

---
+++ SNDTRCK/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SNDTRCK.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+	[Route("Admin/[controller]/[action]")]
+	public class DashboardController: Controller
+    {
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
