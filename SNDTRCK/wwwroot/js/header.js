3ba997a93a866c59f65774eab77b8592d004f777
Franslin <frans.lindstrom.webd23jon@edu.tucsweden.se>
2024-04-23 18:55:49 +0200

    Working on header

---
+++ SNDTRCK/wwwroot/js/header.js
@@ -0,0 +1,13 @@
+﻿
+
+//To make the navbar dissapear when scrolling down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}   
