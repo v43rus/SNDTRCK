ed2fb1cbd812117b2824d0b1796a77418d127ba2
v43rus <leonard.jm@live.com>
2024-04-26 10:22:09 +0200

    Now uploads file and sets coverimglink on the album in db. Files also get deleted from the wwrwoot when deleting the album

--- SNDTRCK/Areas/Admin/Controllers/ProductController.cs
+++ SNDTRCK/Areas/Admin/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SNDTRCK.Controllers;
 using SNDTRCK.Models;
+using System.Drawing;
 
 namespace SNDTRCK.Areas.Admin.Controllers
 {
@@ -14,12 +15,14 @@ namespace SNDTRCK.Areas.Admin.Controllers
 	{
 		private readonly ILogger<HomeController> _logger;
 		private readonly SNDTRCKContext _context;
+		private readonly IWebHostEnvironment _hostingEnvironment;
 
 
-		public ProductController(SNDTRCKContext context, ILogger<HomeController> logger)
+		public ProductController(SNDTRCKContext context, IWebHostEnvironment hostingEnvironment, ILogger<HomeController> logger)
 		{
 			_logger = logger;
 			_context = context;
+			_hostingEnvironment = hostingEnvironment;
 		}
 
 		public IActionResult ManageProducts()
@@ -28,17 +31,34 @@ namespace SNDTRCK.Areas.Admin.Controllers
 			return View(products);
 		}
 
-		public IActionResult SaveNewProduct()
+		[HttpPost]
+		public async Task<IActionResult> SaveNewProduct(IFormFile image)
 		{
 			var product = new Product()
 			{
 				Title = Request.Form["title"],
 				Artist = Request.Form["artist"],
 				Genre = Request.Form["genre"],
-				Description = "",
+				Description = Request.Form["description"],
 				Price = Decimal.Parse(Request.Form["price"]!),
-				CoverImageLink = "",
 			};
+
+			if (image != null)
+			{
+				var uploadsDirectory = Path.Combine(_hostingEnvironment.WebRootPath, "media/pictures/album-covers");
+				Guid guid = Guid.NewGuid();
+				var filePath = Path.Combine(uploadsDirectory, guid + Path.GetExtension(image.FileName));
+
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					product.CoverImageLink = $"media/pictures/album-covers/{guid + Path.GetExtension(image.FileName)}";
+					await image.CopyToAsync(stream);
+				}
+			}
+			else
+			{
+				ModelState.AddModelError(string.Empty, "Invalid image format");
+			}
 
 			_context.Add(product);
 			_context.SaveChanges();
@@ -46,9 +66,20 @@ namespace SNDTRCK.Areas.Admin.Controllers
 			return RedirectToAction("ManageProducts");
 		}
 
-		public IActionResult DeleteProduct(int productId)
+		public IActionResult DeleteProduct(int? productId)
 		{
+			if (productId == null)
+				return NotFound();
+
 			var productToRemove = _context.Products.FirstOrDefault(p => p.ProductId == productId);
+
+			var filePath = _hostingEnvironment.WebRootPath;
+			filePath += $"/{productToRemove!.CoverImageLink!}";
+
+			if (System.IO.File.Exists(filePath))
+				System.IO.File.Delete(filePath);
+
+
 
 			if (productToRemove != null)
 			{ 
