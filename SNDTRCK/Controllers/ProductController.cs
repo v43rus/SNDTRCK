b59ebcdbc8bfe419d0c7ea832e40f91cebe31e64
v43rus <leonard.jm@live.com>
2024-05-22 11:22:20 +0200

    Created product controller where the API requests will be processed and used to fetch information.

---
+++ SNDTRCK/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SNDTRCK.Models;
+
+namespace SNDTRCK.Controllers
+{
+	public class ProductController : Controller
+	{
+		/*
+			Consumer Key: AiPjOXzHDfBidtypjDMy
+		    Consumer Secret: kazcSyuJSuoRkXRVJEnUWnXgOombLicA
+		*/
+
+		// Sample request from API
+		// https://api.discogs.com/database/search?title=abbey_road&artist=beatles&key=AiPjOXzHDfBidtypjDMy&secret=kazcSyuJSuoRkXRVJEnUWnXgOombLicA
+
+		private readonly ILogger<HomeController> _logger;
+		private readonly SNDTRCKContext _context;
+
+
+		public ProductController(SNDTRCKContext context, ILogger<HomeController> logger)
+		{
+			_logger = logger;
+			_context = context;
+		}
+
+		public IActionResult Product(int? productId)
+		{
+
+
+			ViewBag.Product = _context.Products.Find(productId)!;
+			return View();
+		}
+	}
+}
