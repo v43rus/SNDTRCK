4bee456f3533a768625d923d600061006b8aca07
v43rus <leonard.jm@live.com>
2024-05-22 13:08:12 +0200

    Working on API

--- SNDTRCK/Controllers/ProductController.cs
+++ SNDTRCK/Controllers/ProductController.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Drawing.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using SNDTRCK.Models;
 
 namespace SNDTRCK.Controllers
 {
 	public class ProductController : Controller
 	{
-		/*
-			Consumer Key: AiPjOXzHDfBidtypjDMy
-		    Consumer Secret: kazcSyuJSuoRkXRVJEnUWnXgOombLicA
-		*/
-
 		// Sample request from API
 		// https://api.discogs.com/database/search?title=abbey_road&artist=beatles&key=AiPjOXzHDfBidtypjDMy&secret=kazcSyuJSuoRkXRVJEnUWnXgOombLicA
 
 		private readonly ILogger<HomeController> _logger;
 		private readonly SNDTRCKContext _context;
+		public String apiPath = "https://api.discogs.com/database/search?";
 
+		// Variables for building API Call
+		private String title = "title=";
+		private String artist = "&artist=";
+		private String consumerKey = "&key=AiPjOXzHDfBidtypjDMy";
+		private String consumerSecret = "&secret=kazcSyuJSuoRkXRVJEnUWnXgOombLicA";
+
+		// Json Object
 
 		public ProductController(SNDTRCKContext context, ILogger<HomeController> logger)
 		{
@@ -23,12 +28,35 @@ namespace SNDTRCK.Controllers
 			_context = context;
 		}
 
-		public IActionResult Product(int? productId)
+		public async Task<IActionResult> Product(int? productId)
 		{
+			// Assign product
+			var product = await _context.Products.FindAsync(productId);
+			if (product == null)
+			{
+				return NotFound(); // Handle case where product is not found
+			}
 
+			// BUILD STRING FOR API CALL
+			title += product.Title.Replace(' ', '-');
+			artist += product.Artist.Replace(' ', '-');
 
-			ViewBag.Product = _context.Products.Find(productId)!;
-			return View();
+			apiPath += (title + artist + consumerKey + consumerSecret);
+
+			// Make HTTP request
+			using (var httpClient = new HttpClient())
+			{
+				var response = await httpClient.GetStringAsync(apiPath);
+				var album = JObject.Parse(response);
+
+				Console.WriteLine(apiPath);
+
+				ViewBag.Product = product;
+				ViewBag.Album = album; // Pass the album data to the view if needed
+
+				return View();
+			}
 		}
+
 	}
 }
