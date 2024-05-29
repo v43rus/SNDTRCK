d82f638150c1d839ff91efeffb732b3c86254c7e
v43rus <leonard.jm@live.com>
2024-05-29 07:17:13 +0200

    Fixed mobile nav

---
+++ SNDTRCK/wwwroot/js/fixedNav.js
@@ -0,0 +1,20 @@
+﻿function updateNavPosition() {
+    var header = document.getElementById('header');
+    var fixedElement = document.getElementById('nav');
+        
+    // Get the height of the header
+    var headerHeight = header.offsetHeight;
+
+    // Calculate the height for the nav to fill the remaining space
+    var navHeight = window.innerHeight - headerHeight;
+
+    // Set the height of the nav
+    nav.style.top = headerHeight + 'px';
+    nav.style.height = navHeight + 'px';
+}
+
+// Call the function initially to set the position
+updateNavPosition();
+
+// Update the position on window resize
+window.addEventListener('resize', updateNavPosition);
