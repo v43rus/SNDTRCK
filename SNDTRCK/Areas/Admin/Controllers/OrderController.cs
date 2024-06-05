066aff2144289416fbdc5d40d021b9e2460d4501
v43rus <leonard.jm@live.com>
2024-06-05 12:12:40 +0200

    Manage orders almost done

--- SNDTRCK/Areas/Admin/Controllers/OrderController.cs
+++ SNDTRCK/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SNDTRCK.Controllers;
@@ -10,26 +11,48 @@ using System.Drawing;
 
 namespace SNDTRCK.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Route("Admin/[controller]/[action]")]
-    [Authorize(Roles = "Admin")]
-    public class OrderController : Controller
-    {
-	    private readonly ILogger<HomeController> _logger;
-	    private readonly SNDTRCKContext _context;
+	[Area("Admin")]
+	[Route("Admin/[controller]/[action]")]
+	[Authorize(Roles = "Admin")]
+	public class OrderController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly SNDTRCKContext _context;
 
 
-	    public OrderController(SNDTRCKContext context, ILogger<HomeController> logger)
-	    {
-		    _logger = logger;
-		    _context = context;
-	    }
+		public OrderController(SNDTRCKContext context, ILogger<HomeController> logger)
+		{
+			_logger = logger;
+			_context = context;
+		}
 		public IActionResult ManageOrders()
 		{
 			ViewBag.Orders = _context.Orders.ToList();
 			ViewBag.OrderDetails = _context.OrderDetails.ToList();
 			
-            return View();
-        }
-    }
+			return View();
+		}
+
+		public async Task<IActionResult> EditOrder(int? orderId)
+		{
+			var order = await _context.Orders.Where(o => o.OrderId == orderId).FirstOrDefaultAsync();
+			var orderDetails = _context.OrderDetails.Where(o => o.OrderId == orderId).ToList();
+
+			List<Product> products = new List<Product>();
+
+			for (int i = 0; i < orderDetails.Count; i++)
+			{
+				var tempProduct = _context.Products.Where(p => p.ProductId == orderDetails[i].ProductId)
+					.FirstOrDefault();
+
+				products.Add(tempProduct);
+			}
+
+			ViewBag.Order = order!;
+			ViewBag.OrderDetails = _context.OrderDetails.Where(o => o.OrderId == orderId).ToList();
+			ViewBag.Products = products;
+
+			return View();
+		}
+	}
 }
--- SNDTRCK/Models/Order.cs
+++ SNDTRCK/Models/Order.cs
@@ -1,33 +1,34 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace SNDTRCK.Models;
 
 public partial class Order
 {
-    public int OrderId { get; set; }
+	public int OrderId { get; set; }
 
-    public string FirstName { get; set; } = null!;
+	public string FirstName { get; set; } = null!;
 
-    public string LastName { get; set; } = null!;
+	public string LastName { get; set; } = null!;
 
-    public string Address { get; set; } = null!;
+	public string Address { get; set; } = null!;
 
-    public string PostalCode { get; set; } = null!;
+	public string PostalCode { get; set; } = null!;
 
-    public string City { get; set; } = null!;
+	public string City { get; set; } = null!;
 
-    public string PhoneNumber { get; set; } = null!;
+	public string PhoneNumber { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+	public string Email { get; set; } = null!;
 
-    public int Quantity { get; set; }
+	public int Quantity { get; set; }
 
-    public decimal OrderSum { get; set; }
+	public decimal OrderSum { get; set; }
 
-    public DateTime OrderDate { get; set; }
+	public DateTime OrderDate { get; set; }
 
-    public string? UserId { get; set; }
+	public string? UserId { get; set; }
 
-    public string? OrderStatus { get; set; }
+	public string? OrderStatus { get; set; }
 }
